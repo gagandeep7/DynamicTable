8d2820834cd2af4c72201e1f0e6bb351c272949d
Gagandeep <gag7550@gmail.com>
2020-06-17 15:57:14 +0530

    Added Search

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Select table heading
 const tHead = document.querySelector("table thead");
 const tBody = document.querySelector("table tbody");
-
+const Input = document.querySelector("input");
 // Heading data
 const Headings = [
   {
@@ -54,7 +54,7 @@ for (let data of Headings) {
   // get td of headings
   Object.values(data).map(
     //   get values of columns
-    (headings) => (headingRow += `<td>${headings}</td>`)
+    (headings) => (headingRow += `<th>${headings}</th>`)
   );
   //   end the row tag
   headingRow += "</tr>";
@@ -69,3 +69,35 @@ for (let student of Body) {
   BodyRow += "</tr>";
   tBody.innerHTML += BodyRow;
 }
+
+// Searching
+
+// Get the input data
+Input.addEventListener("keyup", (e) => {
+  // get all rows data
+  const TableRows = document.querySelectorAll("tbody > tr");
+
+  // Input value
+  let InputData = e.target.value.toUpperCase();
+  // SEARCH ROW WISE
+  // get tds from rows
+  for (let i = 0; i < TableRows.length; i++) {
+    // Intially hide all rows
+    TableRows[i].style.display = "none";
+    // store All TDS from a  row
+    let Tds = TableRows[i].querySelectorAll("td");
+    // SEARCH COLUMN WISE
+    for (let j = 0; j < Tds.length; j++) {
+      // Check individual td inside row
+      let cell = Tds[j];
+      if (cell) {
+        // If td content is same as Searched
+        if (cell.innerText.toUpperCase().indexOf(InputData) > -1) {
+          // Show Table row
+          TableRows[i].style.display = "";
+          break;
+        }
+      }
+    }
+  }
+});
